cd7a657a41a1273560feb35bb39dce9dffe28cae
Cassie Treece <treecal20@gmail.com>
2022-10-17 10:13:08 -0700

    update pipeline names

--- TCDataProcessing/pipelines/CycloneHistogram_P.py
+++ TCDataProcessing/pipelines/CycloneHistogram_P.py
@@ -5,7 +5,7 @@ from TCDataCollection.models import Resource
 from TCDataProcessing.models import Storm, Sensor
 from TCFrontEnd.models import Product
 
-def P_StormCenteredTrack(storm, resources, products_IN, output_path, output_filename):
+def P_CycloneHistogram(storm, resources, products_IN, output_path, output_filename):
     reduced_track = None
     AE_path = None
     storm_name = storm.name
@@ -20,18 +20,13 @@ def P_StormCenteredTrack(storm, resources, products_IN, output_path, output_file
             dir = resource.collect(storm, sensor.mission, sensor)
             if resource.name == 'AEFiles' and dir:
                 AE_path = dir
-    
+                
+
     if not output_path:
         output_path = str(file_io.get_parent(reduced_track))
-    
     if not output_filename:
-        output_filename = 'Storm_Centered_Locations.txt'
+        output_filename = "Histogram.jpg"
+    if not storm_name:
+        storm_name = "NO_NAME"
     
-    output_path = file_io.create_path(output_path, output_filename)
-    run_matlab_script('StormCenteredLocations','r"{}", r"{}", r"{}"'.format(reduced_track, AE_path, output_path))
-    
-# example:
-# from TCDataProcessing.pipelines import StormCenteredTrack as sct
-# sct.P_StormCenteredTrack(r'E:\Users\MetaTek\Documents\Projs\wwlln\wwlln\data\Rai\trackfile.txt',
-#                           r'E:\Users\MetaTek\Documents\Projs\wwlln\wwlln\data\Rai')
-
+    run_matlab_script('CycloneHistogram','r"{}", r"{}", r"{}", r"{}", r"{}"'.format(reduced_track, AE_path, storm_name, output_path, output_filename))
